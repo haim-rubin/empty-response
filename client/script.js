1d147871d5b216b133e2a034aafaca913e5db0d5
Haim Rubin <haim.rubin@gmail.com>
2018-04-24 15:40:49 +0300

    Test fetch cancel

--- client/script.js
+++ client/script.js
@@ -25,8 +25,8 @@
     })
   )
 
-  const fetchRequest = (api) => (
-    fetch(api)
+  const fetchRequest = (api, options) => (
+    fetch(api, options)
       .then(res => res.json())
   )
 
@@ -34,15 +34,18 @@
   const xhrButton = document.getElementById(`${'btnXHR'}`)
   const container = document.getElementById(`${'container'}`)
 
-  const getWith = request => Promise.all([
-      request("/api/data1?seconds=2"),
-      request("/api/data2?seconds=2"),
-      request("/api/data3?seconds=2")
+  const getWith = (request, options) => Promise.all([
+      request("/api/data1?seconds=2", options),
+      request("/api/data2?seconds=2", options),
+      request("/api/data3?seconds=2", options)
     ]).then(([{ data: d1 }, { data: d2 }, { data: d3 }]) => [d1, d2, d3]);
+
   const attach = (button, request, container, message, milli = 1500) => {
     button.addEventListener('click', (e) => {
       container.innerHTML = ''
-      getWith(request)
+      const controller = new AbortController();
+      const { signal } = controller
+      getWith(request, { signal })
         .then(([d1, d2, d3]) => {
           container.innerHTML = `${d1}${d2}${d3}`;
         })
@@ -51,11 +54,12 @@
         });
 
       setTimeout(() => {
-        location.reload();
+        controller.abort();
+        //location.reload();
       }, milli)
     })
   }
 
-  attach(fetchButton, fetchRequest, container, `This alert raised from in fetch it's catch when refreshig the page during fetch call, with an empty error`, 1500);
+  attach(fetchButton, fetchRequest, container, `This alert raised from in fetch it's catch when refreshig the page during fetch call, with an empty error`, 1000);
   attach(xhrButton, xhrRequest, container, `This alert raised from in XmlHttpRequest it's catch when refreshig the page during fetch call, with an empty error`, 2000);
 })()
